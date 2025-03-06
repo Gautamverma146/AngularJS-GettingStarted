8c1ae8d615a51dd3e33d5ab3d090571d25bd82c2
Gautamverma2003 <gautam146official@gmail.com>
2025-03-06 17:03:05 +0530

    [Gautam] added Ensure user name is valid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,22 @@ export class AppComponent {
   imgURL = "../assets/BL_logo_square_png.png"
   url = "https://www.bridgelabz.com/"
   userName: string = "";
-  onClick(){
+  nameError: string= "";
+  ngOnInit(): void{
+    this.title= "Hello From Bridgelabz."
+  }
+  onClick($event:any){
+    console.log("Save button is clicked",  $event);
     window.open(this.url)
+  }
+  onInput($event:any){
+    console.log("Change Event Occurred!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError="";
+      return;
+    }
+    this.nameError="Name is Incorrect!";
   }
 
 }
